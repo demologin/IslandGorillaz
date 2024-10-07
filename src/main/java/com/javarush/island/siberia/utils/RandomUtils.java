c2e9233949bde9e619dc8981442b1c246adf1923
Meeeat <angroov@gmail.com>
2024-10-08 00:36:37 +0900

    REF add checks to random util for < 0

--- src/main/java/com/javarush/island/siberia/utils/RandomUtils.java
+++ src/main/java/com/javarush/island/siberia/utils/RandomUtils.java
@@ -4,14 +4,23 @@ import java.util.concurrent.ThreadLocalRandom;
 
 public class RandomUtils {
     public static double randomDouble(double min, double max) {
+        if (min >= max) {
+            throw new IllegalArgumentException("min must be > max");
+        }
         return ThreadLocalRandom.current().nextDouble(min, max);
     }
 
     public static int randomInt(int min, int max) {
+        if (min >= max) {
+            throw new IllegalArgumentException("min must be > max");
+        }
         return ThreadLocalRandom.current().nextInt(min, max + 1);
     }
 
     public static boolean chance(int percent) {
+        if (percent < 0 || percent > 100) {
+            throw new IllegalArgumentException("percent must be from 0 to 100");
+        }
         return ThreadLocalRandom.current().nextInt(100) < percent;
     }
 
