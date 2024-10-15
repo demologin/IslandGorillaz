d4a75eff3ce403de8601f701d137b2d28ac0f0d7
Meeeat <angroov@gmail.com>
2024-10-16 01:05:04 +0900

    ADD reproduce method

--- src/main/java/com/javarush/island/siberia2/api/Reproducible.java
+++ src/main/java/com/javarush/island/siberia2/api/Reproducible.java
@@ -1,5 +1,6 @@
 package com.javarush.island.siberia2.api;
 
+@FunctionalInterface
 public interface Reproducible {
     void reproduce();
 }
--- src/main/java/com/javarush/island/siberia2/services/ReproductionService.java
+++ src/main/java/com/javarush/island/siberia2/services/ReproductionService.java
@@ -1,4 +1,30 @@
 package com.javarush.island.siberia2.services;
 
+import com.javarush.island.siberia2.entity.animals.Animal;
+
 public class ReproductionService {
+
+    public void reproduce(Animal animal) {
+        if (animal.getCurrentFoodLevel() < animal.getSettings().getMaxFood()) {
+            return;
+        }
+
+        long partners = animal.getCurrentCell().getAnimals().stream()
+                .filter(a -> a.getClass() == animal.getClass() && a != animal)
+                .count();
+        if (partners > 0
+                && animal.getCurrentCell().getAnimals().size()
+                < animal.getSettings().getMaxCountPerCell()) {
+            try {
+                Animal offspring = animal.clone();
+                offspring.setCurrentFoodLevel(offspring.getSettings().getMaxFood() / 2);
+                offspring.setCurrentCell(animal.getCurrentCell());
+                animal.getCurrentCell().addAnimal(offspring);
+                animal.setCurrentFoodLevel(animal.getCurrentFoodLevel()
+                        - animal.getSettings().getMaxFood() / 2);
+            } catch (CloneNotSupportedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
 }
