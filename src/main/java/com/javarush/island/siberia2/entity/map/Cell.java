fd32c71f24e4a2d2a6a61026a8ff38e2dcef5e91
Meeeat <angroov@gmail.com>
2024-10-16 01:13:15 +0900

    REF replace neighbor cell method

--- src/main/java/com/javarush/island/siberia2/entity/map/Cell.java
+++ src/main/java/com/javarush/island/siberia2/entity/map/Cell.java
@@ -1,19 +1,22 @@
 package com.javarush.island.siberia2.entity.map;
 
 import com.javarush.island.siberia2.entity.animals.Animal;
+import com.javarush.island.siberia2.entity.plants.Plant;
 import lombok.Getter;
 import java.util.*;
-import java.util.concurrent.ConcurrentLinkedQueue;
+import java.util.concurrent.CopyOnWriteArrayList;
 
 public class Cell {
 
-    private final int x;
-    private final int y;
-    private boolean isWater;
     @Getter
     private final Island island;
     @Getter
-    private final Queue<Animal> animals = new ConcurrentLinkedQueue<>();
+    private final int x;
+    @Getter
+    private final int y;
+    private final boolean isWater;
+    private final CopyOnWriteArrayList<Animal> animals = new CopyOnWriteArrayList<>();
+    private final CopyOnWriteArrayList<Plant> plants = new CopyOnWriteArrayList<>();
 
     public Cell(int x, int y, boolean isWater, Island island) {
         this.x = x;
@@ -22,8 +25,8 @@ public class Cell {
         this.island = island;
     }
 
-    public Collection<Animal> getAnimals() {
-        return Collections.unmodifiableCollection(new ArrayList<>(animals));
+    public List<Animal> getAnimals() {
+        return Collections.unmodifiableList(animals);
     }
 
     public void addAnimal(Animal animal) {
@@ -34,57 +37,36 @@ public class Cell {
         animals.remove(animal);
     }
 
+    public List<Plant> getPlants() {
+        return Collections.unmodifiableList(plants);
+    }
+
+    public void addPlant(Plant plant) {
+        plants.add(plant);
+    }
+
+    public void removePlant(Plant plant) {
+        plants.remove(plant);
+    }
+
     public boolean isWater() {
         return isWater;
     }
 
-    public List<Cell> getAdjacentCells() {
-        List<Cell> neighbors = new ArrayList<>();
-        int width = island.getWidth();
-        int height = island.getHeight();
-
-        if (x > 0) {
-            neighbors.add(island.getCell(x - 1, y)); //⬅️
-            if (y > 0) {
-                neighbors.add(island.getCell(x - 1, y - 1)); //↖️
-            }
-            if (y < height - 1) {
-                neighbors.add(island.getCell(x - 1, y + 1)); //↙️
-            }
-        }
-        if (x < width - 1) {
-            neighbors.add(island.getCell(x + 1, y)); //➡️
-            if (y > 0) {
-                neighbors.add(island.getCell(x + 1, y - 1)); //↗️
-            }
-            if (y < height - 1) {
-                neighbors.add(island.getCell(x + 1, y + 1)); //↘️
-            }
-        }
-        if (y > 0) {
-            neighbors.add(island.getCell(x, y - 1)); //⬆️
-        }
-        if (y < height - 1) {
-            neighbors.add(island.getCell(x, y + 1)); //⬇️
-        }
-
-        return neighbors;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-
-        Cell cell = (Cell) o;
-
-        if (x != cell.x) return false;
-        return y == cell.y;
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(x, y);
-    }
+//    @Override
+//    public boolean equals(Object o) {
+//        if (this == o) return true;
+//        if (o == null || getClass() != o.getClass()) return false;
+//
+//        Cell cell = (Cell) o;
+//
+//        if (x != cell.x) return false;
+//        return y == cell.y;
+//    }
+//
+//    @Override
+//    public int hashCode() {
+//        return Objects.hash(x, y);
+//    }
 
 }
--- src/main/java/com/javarush/island/siberia2/services/MovementService.java
+++ src/main/java/com/javarush/island/siberia2/services/MovementService.java
@@ -3,17 +3,18 @@ package com.javarush.island.siberia2.services;
 import com.javarush.island.siberia.utils.RandomUtils;
 import com.javarush.island.siberia2.entity.animals.Animal;
 import com.javarush.island.siberia2.entity.map.Cell;
-
 import java.util.List;
 
 public class MovementService {
+
+    private final AdjacentCellService adjacentCellService = new AdjacentCellService();
 
     public void move(Animal animal) {
         int speed = animal.getSettings().getSpeed();
         Cell currentCell = animal.getCurrentCell();
 
         for (int i = 0; i < speed; i++) {
-            List<Cell> adjacentCells = currentCell.getAdjacentCells();
+            List<Cell> adjacentCells = adjacentCellService.getAdjacentCells(currentCell);
 
             List<Cell> possibleCells = adjacentCells.stream()
                     .filter(cell -> !cell.isWater())
