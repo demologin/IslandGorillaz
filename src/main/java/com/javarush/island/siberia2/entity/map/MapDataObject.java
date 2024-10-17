7b1902f602045281dde3afed77874949dc60fbea
Meeeat <angroov@gmail.com>
2024-10-17 21:52:21 +0900

    REF cut MapDataObject to 3 and add collision checker

--- src/main/java/com/javarush/island/siberia2/entity/map/MapDataObject.java
+++ src/main/java/com/javarush/island/siberia2/entity/map/MapDataObject.java
@@ -1,25 +1,33 @@
 package com.javarush.island.siberia2.entity.map;
 
-import com.javarush.island.siberia2.config.Constants;
 import com.javarush.island.siberia2.entity.map.generators.ObjectType;
-import com.javarush.island.siberia2.entity.map.generators.TerrainType;
+import com.javarush.island.siberia2.entity.map.generators.mapUtil.PlacementChecker;
+import com.javarush.island.siberia2.entity.map.generators.RockPlacer;
+import com.javarush.island.siberia2.entity.map.generators.FieldGenerator;
+import com.javarush.island.siberia2.entity.map.generators.ForestGenerator;
 import lombok.Getter;
 import java.util.Random;
 
 public class MapDataObject {
-
     @Getter
     private final ObjectType[][] objectMap;
     private final int width;
     private final int height;
-    private final Random random = new Random();
-    private final MapDataTerrain terrainData;
+    private final RockPlacer rockPlacer;
+    private final FieldGenerator fieldGenerator;
+    private final ForestGenerator forestGenerator;
 
     public MapDataObject(int width, int height, MapDataTerrain terrainData) {
         this.width = width;
         this.height = height;
-        this.terrainData = terrainData;
-        objectMap = new ObjectType[height][width];
+        this.objectMap = new ObjectType[height][width];
+
+        PlacementChecker placementChecker = new PlacementChecker(objectMap, terrainData, width, height);
+        Random random = new Random();
+        this.rockPlacer = new RockPlacer(objectMap, placementChecker, random);
+        this.fieldGenerator = new FieldGenerator(objectMap, placementChecker, random);
+        this.forestGenerator = new ForestGenerator(objectMap, placementChecker, random);
+
         generateObjectMap();
     }
 
@@ -29,94 +37,9 @@ public class MapDataObject {
                 objectMap[y][x] = ObjectType.NO_OBJECT;
             }
         }
-        placeRocks();
-        generateFields();
-        generateForests();
-    }
-
-    private void placeRocks() {
-        int rockCount = (width * height) / 50;
-        for (int i = 0; i < rockCount; i++) {
-            int x = random.nextInt(width);
-            int y = random.nextInt(height);
-            if (isValidPlacement(x, y)) {
-                objectMap[y][x] = ObjectType.ROCK;
-            }
-        }
-    }
-
-    private void generateFields() {
-        int fieldCount = Constants.FIELD_COUNT;
-        int fieldSize = Constants.FIELD_SIZE;
-
-        for (int i = 0; i < fieldCount; i++) {
-            int centerX = random.nextInt(width);
-            int centerY = random.nextInt(height);
-
-            for (int y = centerY - fieldSize; y <= centerY + fieldSize; y++) {
-                for (int x = centerX - fieldSize; x <= centerX + fieldSize; x++) {
-                    if (isValidPlacement(x, y) && isAdjacentToObject(x, y, ObjectType.TREE)) {
-                        if (random.nextInt(100) < 80) {
-                            objectMap[y][x] = ObjectType.WHEAT;
-                        }
-                    }
-                }
-            }
-        }
-    }
-
-    private void generateForests() {
-        int forestCount = Constants.FOREST_COUNT;
-        int forestSize = Constants.FOREST_SIZE;
-
-        for (int i = 0; i < forestCount; i++) {
-            int centerX = random.nextInt(width);
-            int centerY = random.nextInt(height);
-
-            for (int y = centerY - forestSize; y <= centerY + forestSize; y++) {
-                for (int x = centerX - forestSize; x <= centerX + forestSize; x++) {
-                    if (isValidPlacement(x, y) && isAdjacentToObject(x, y, ObjectType.WHEAT)) {
-                        if (random.nextInt(100) < 70) {
-                            objectMap[y][x] = ObjectType.TREE;
-                        }
-                    }
-                }
-            }
-        }
-    }
-
-    private boolean isValidPlacement(int x, int y) {
-        if (!isInBounds(x, y)) {
-            return false;
-        }
-
-        TerrainType tile = terrainData.getTile(x, y);
-        ObjectType object = objectMap[y][x];
-
-        return tile != TerrainType.WATER
-                && tile != TerrainType.PATH
-                && tile != TerrainType.BRIDGE
-                && object == ObjectType.NO_OBJECT;
-    }
-
-    private boolean isAdjacentToObject(int x, int y, ObjectType objectType) {
-        for (int dy = -1; dy <= 1; dy++) {
-            for (int dx = -1; dx <= 1; dx++) {
-                if (dx == 0 && dy == 0) continue;
-                int nx = x + dx;
-                int ny = y + dy;
-                if (isInBounds(nx, ny)) {
-                    if (objectMap[ny][nx] == objectType) {
-                        return false;
-                    }
-                }
-            }
-        }
-        return true;
-    }
-
-    private boolean isInBounds(int x, int y) {
-        return x >= 0 && x < width && y >= 0 && y < height;
+        rockPlacer.placeRocks();
+        fieldGenerator.generateFields();
+        forestGenerator.generateForests();
     }
 
 }
